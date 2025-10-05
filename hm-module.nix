047bff906979b79cc16d37163de4d99828d9f8bc
toyboot4e <toyboot4e@gmail.com>
2025-10-05 22:34:21 +0900

    is this the cause

--- hm-module.nix
+++ hm-module.nix
@@ -133,10 +133,7 @@ in
         }
         # Linux
         (lib.mkIf (cfg.settings != null && pkgs.stdenvNoCC.isLinux) (
-          if builtins.getEnv "XDG_CONFIG_HOME" != "" then
-            { lib.xdg.configFile."plover/plover.cfg_xdg".source = configFile; }
-          else
-            { home.file.".config/plover/plover.cfg_default".source = configFile; }
+          { lib.xdg.configFile."plover/plover.cfg".source = configFile; }
         ))
         # macOS
         (lib.mkIf (cfg.settings != null && pkgs.stdenvNoCC.isDarwin) {
